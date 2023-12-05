f6e83546838c7b7efe77fc4820784400f8151759
JonathanBC2 <147190800+JonathanBC2@users.noreply.github.com>
2023-12-05 16:07:24 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,8 @@ const preloader = document.querySelector("[data-preaload]");
 
 window.addEventListener("load", function () {
     preloader.classList.add("loaded");
-    this.document.body.classList.add("loaded")
-})
+    document.body.classList.add("loaded");
+});
 
 
 
